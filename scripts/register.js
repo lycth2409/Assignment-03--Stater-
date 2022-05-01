55430123895f1b761bfd628cce01e4b9d275ff17
Cap Thi Hai Ly <hailylydk@gmail.com>
2022-05-02 00:34:12 +0700

    check duplicate username

--- scripts/register.js
+++ scripts/register.js
@@ -2,34 +2,50 @@
 
 function store() {
     let userArray = JSON.parse(localStorage.getItem('userArray')) || []
- 
+
     let firstname = document.getElementById('input-firstname').value;
     let lastname = document.getElementById('input-lastname').value;
     let username = document.getElementById('input-username').value;
     let password = document.getElementById('input-password').value;
     let passwordConfirm = document.getElementById('input-password-confirm').value;
-
-    let user = {firstname,lastname,username,password}
+    localStorage.setItem('username', JSON.stringify(username)); //stringify object and store
+    var retrievedUsername = JSON.parse(localStorage.getItem('username')); //retrieve the object
+    let user = { firstname, lastname, username, password }
 
     if (user.firstname.length == 0) {
         alert('Please fill in first name');
-    } else if (user.lastname.length == 0) {
+    } 
+    else if (user.lastname.length == 0) 
+    {
         alert('Please fill in last name');
-    } else if (user.username.length == 0) {
+    } 
+    else if (user.username.length == 0)
+    {
         alert('Please fill in user name');
-    } else if (user.password.length == 0) {
+    } 
+    else if (re === user.username) 
+    {
+        alert('username already exists');
+    }
+    else if (user.password.length == 0) 
+    {
         alert('Please fill in password');
-    } else if (passwordConfirm.length == 0) {
+    } 
+    else if (passwordConfirm.length == 0) 
+    {
         alert('Please fill in password confirm');
-    } else if (user.password.length < 8) {
+    } 
+    else if (user.password.length < 8) 
+    {
         alert('Password must have more than 8 char');
-    } else if (user.password != passwordConfirm) {
+    } 
+    else if (user.password != passwordConfirm) 
+    {
         alert('Password confirm not match');
-    } else {
+    } 
+    else {
         userArray.push(user)
         localStorage.setItem('userArray', JSON.stringify(userArray));
         alert('Your account has been created');
     }
 }
-
-
